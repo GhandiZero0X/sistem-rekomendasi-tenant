ec3d68101313ecdf0fe104a4479163711f5cef24
Muhamad Ghandi Nur Setiawan <134348061+GhandiZero0X@users.noreply.github.com>
2025-09-29 14:16:57 +0700

    add authController, JWT utility and middlewares

--- controllers/authController.py
+++ controllers/authController.py
@@ -1,3 +1,14 @@
+# controllers/authController.py
 import os
 import pandas as pd
 
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+DATA_DIR = os.path.join(BASE_DIR, "../data")
+
+USERPATH = os.path.join(DATA_DIR, "users.csv")
+
+# login akun
+
+# register akun
+
+# approval akun admin oleh superadmin
--- services/init_admin.py
+++ services/init_admin.py
@@ -1,24 +1,27 @@
-# services/init_admin.py
 import csv
 import os
 import bcrypt
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 DATA_DIR = os.path.join(BASE_DIR, "data")
-ADMIN_PATH = os.path.join(DATA_DIR, "admins.csv")
+ADMIN_PATH = os.path.join(DATA_DIR, "users.csv")
 
 def create_admin_csv():
     if not os.path.exists(DATA_DIR):
         os.makedirs(DATA_DIR)
 
-    if os.path.exists(ADMIN_PATH):
-        print("⚠️ File admins.csv sudah ada.")
-        return
+    # if os.path.exists(ADMIN_PATH):
+    #     print("⚠️ File admins.csv sudah ada.")
+    #     return
 
-    # contoh admin default
+    # pembuatan akun super admin
     admin_users = [
-        {"id": 1, "username": "admin", "password": "admin123"},
-        {"id": 2, "username": "superadmin", "password": "super123"}
+        {
+            "id": 1,
+            "username": "superadmin@injourney",
+            "password": "super@juandaInjourney123",
+            "status_approval": 1
+        }
     ]
 
     # hash password
@@ -28,11 +31,11 @@ def create_admin_csv():
 
     # tulis ke CSV
     with open(ADMIN_PATH, mode="w", newline="", encoding="utf-8") as file:
-        writer = csv.DictWriter(file, fieldnames=["id", "username", "password"])
+        writer = csv.DictWriter(file, fieldnames=["id", "username", "password", "status_approval"])
         writer.writeheader()
         writer.writerows(admin_users)
 
-    print(f"✅ File {ADMIN_PATH} berhasil dibuat dengan admin default.")
+    print(f"✅ File {ADMIN_PATH} berhasil dibuat dengan admin default (status_approval=0).")
 
 if __name__ == "__main__":
     create_admin_csv()
