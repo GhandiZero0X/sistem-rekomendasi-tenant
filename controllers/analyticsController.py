c8778fa12257d627952d79d3a869df7ea27e99d7
Muhamad Ghandi Nur Setiawan <134348061+GhandiZero0X@users.noreply.github.com>
2025-10-01 13:58:29 +0700

    [+] Update analytics controller
    Introduces TENANT_DATA_FILE variable for accessing tenant_preprocessed.csv in the analyticsController. Prepares for future analytics functionality involving tenant data.

--- controllers/analyticsController.py
+++ controllers/analyticsController.py
@@ -4,3 +4,6 @@ import pandas as pd
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 DATA_DIR = os.path.join(BASE_DIR, "../data")
 USER_DATA_FILE = os.path.join(DATA_DIR, "users.csv")
+TENANT_DATA_FILE = os.path.join(DATA_DIR, "tenant_preprocessed.csv")
+
+# Get user analytics
